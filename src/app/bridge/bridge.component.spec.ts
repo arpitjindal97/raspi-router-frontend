78bdcd985b8ca3020676326a117e138de4abe688
Arpit Agarwal <arpitjindal1997@hotmail.com>
2018-04-03 21:32:44 +0530

    testing done for bridge component

--- src/app/bridge/bridge.component.spec.ts
+++ src/app/bridge/bridge.component.spec.ts
@@ -1,6 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BridgeComponent } from './bridge.component';
+import {
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatOptionModule,
+  MatRadioModule,
+  MatSelectModule
+} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CodemirrorModule} from '@ctrl/ngx-codemirror';
+import {RouterTestingModule} from '@angular/router/testing';
+import {IpComponent} from '../ip/ip.component';
+import {DataService} from '../data.service';
 
 describe('BridgeComponent', () => {
   let component: BridgeComponent;
@@ -8,7 +25,25 @@ describe('BridgeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BridgeComponent ]
+      declarations: [
+        IpComponent,
+        BridgeComponent
+      ],
+      imports: [
+        MatFormFieldModule,
+        MatOptionModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatCardModule,
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MatDialogModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatInputModule
+      ],
+      providers: [ DataService ]
     })
     .compileComponents();
   }));
